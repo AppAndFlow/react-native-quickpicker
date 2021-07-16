25d775ae80c36c5277b550b64d4420d34afe83e7
Vince <vdelafontaine15@gmail.com>
2021-07-16 18:18:50 -0400

    bump version

--- src/AndroidPicker.tsx
+++ src/AndroidPicker.tsx
@@ -181,7 +181,6 @@ const AndroidButtonText = ({
 }) => (
   <TouchableOpacity
     onPress={onPress}
-    hitslop={{ top: 20, left: 20, right: 20, bottom: 20 }}
     style={containerStyle}
   >
     <Text
